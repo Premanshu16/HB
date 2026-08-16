a960b739a013e17c92bcc98f430c20983f75e219
Premanshu16 <premanshusahu169@gmail.com>
2026-08-17 01:07:10 +0530

    check cloudinary configuration

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -6,6 +6,12 @@ const connectCloudinary = async () => {
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET,
     });
+
+    console.log("Cloudinary configured:", {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key_exists: !!process.env.CLOUDINARY_API_KEY,
+        api_secret_exists: !!process.env.CLOUDINARY_API_SECRET,
+    });
 };
 
 export default connectCloudinary;
