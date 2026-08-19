af65ab8411a51a745667a5c7135c43dc0a9ff1aa
Premanshu16 <premanshusahu169@gmail.com>
2026-08-19 19:17:42 +0530

    Fix Stripe checkout metadata

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -205,7 +205,7 @@ const line_items = {
         mode: "payment",
         success_url: `${origin}/loader/my-bookings`,
         cancel_url: `${origin}/my-bookings`,
-        matadata:{
+        metadata:{
             bookingId,
         }
     })
